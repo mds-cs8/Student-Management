b418cc2f97fe51bbef914c59c282df0215ce9016
Ahmed Salem <Meed.ssss8@gmail.com>
2022-10-15 00:20:24 +0300

    last edit

--- src/tester/Tester.java
+++ src/tester/Tester.java
@@ -1,61 +1,63 @@
+/**
+ * This program is simply about managing a student system connected to an MYSQL database by a JDBC connector.
+ * Users can add student information,
+ * Read all records,
+ * Searching.
+ *
+ * @author AHMED ALGOWIHI
+ * @author AHMED ALAMOUDI
+ *
+ *
+ */
 package tester;
 
 import java.sql.SQLException;
-
 import java.util.Scanner;
 
-/**
- *
- * @author meed-
- */
 public class Tester {
 
     public static void main(String[] args) throws SQLException {
-        try{
-            
-        
-        Scanner scan = new Scanner(System.in);
-        String urlDB = "jdbc:mysql://localhost:3306/Students_Alamoudi_ALgowihi";
-        String userDB = "ahmed";
-        String password = "0534889865";
-        connectDB connect = new connectDB(urlDB, userDB, password);
-// confugration databsae connection and create object from connectDB class 
+        try {
+            Scanner scan = new Scanner(System.in);
+            String choice;
+            connectDB connect = new connectDB(); // configuration database connection and create an object from connectDB class
 
-        String choice;
-
-        System.out.println("*******************************************************************************");
-        System.out.println("\t\t WELCOME TO STUDENT SYSTEM CREATED BY \n\t\t AHMED ALGOWIHI:438018480 \n\t\t AHMED ALAMOUDI:111111111");
-        System.out.println("*******************************************************************************");
-
-        showMenu();
-        choice = scan.nextLine().trim();
-//        scan.nextLine();
-
-        while (!"4".equals(choice)) {
-            switch (choice) {
-                case "1":
-
-                    connect.addStudent(connect.checkName(), connect.checkDate(), connect.checkGPA());
-                    break;
-                case "2":
-                    connect.readAll();
-                    break;
-                case "3":
-                    connect.search();
-                    break;
-                default:
-                    System.out.println("Enter correct input");
-            }
+            // print  Welcoming message
+            System.out.println("*******************************************************************************");
+            System.out.println("\t\t WELCOME TO STUDENT SYSTEM CREATED BY \n\t\t AHMED ALGOWIHI:438018480 \n\t\t AHMED ALAMOUDI:439006725");
+            System.out.println("*******************************************************************************");
 
             showMenu();
-            choice = scan.next();
-            scan.nextLine();
-        }
-        System.out.println("Thank You For Using Application...");
-        }catch(Exception e){
+            choice = scan.nextLine().trim();
+            // print menu and till user enter number 1-4 
+            while (!"4".equals(choice)) { //start while ,,,,, loop still run until user enter number 4
+                switch (choice) { // start switch
+                    case "1":
+                        connect.addStudent(connect.checkName(), connect.checkDate(), connect.checkGPA()); // if choice #1 >>> call function add student
+                        break;
+
+                    case "2":
+                        connect.readAll(); //if choice #2 >>> call function readAll
+                        break;
+
+                    case "3":
+                        connect.search(); // if choice #3 >>> call function search
+                        break;
+
+                    default: // if enter anything else 
+                        System.out.println("Enter correct input");
+                }// end switch
+
+                showMenu();
+                choice = scan.next();
+                scan.nextLine();
+            } // end while loop
+
+            System.out.println("Thank You For Using Application..."); // //if choice #4  >>> we print this message and close program
+        } catch (Exception e) { // use try and catch to till us if there some problem and print it 
             System.out.println("there is error " + e);
         }
-    }
+    } 
 
     public static void showMenu() {
         System.out.println("**************************************************");
--- src/tester/connectDB.java
+++ src/tester/connectDB.java
@@ -1,86 +1,82 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+/**
+ * in this class we do :
+ *Create connection with database .
+ * Read file contain info about database (URLDB , USER , PASSWORD).
+ * Create 3 methods Add , Read and Search that communicates with the database.
+ * Create 3 methods to handle any problem with name ,date ,GPA
+ *
  */
-//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
 package tester;
 
+import com.mysql.cj.jdbc.Driver;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.Date;
-//import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Properties;
 import java.util.Scanner;
 
-//  @author AHMED ALGOWIHI
-//  @author AHMED ALAMOUDI
 class connectDB {
 
-    String urlDB;
-    String userDB;
-    String password;
-    Connection connect;
     Scanner scan = new Scanner(System.in);
 
-    connectDB(String urlDB, String userDB, String password) throws SQLException {
-        this.urlDB = urlDB;
-        this.userDB = userDB;
-        this.password = password;
-        connect = DriverManager.getConnection(urlDB, userDB, password);
-//         Driver md = new com.mysql.jdbc.Driver();
-//         DriverManager.registerDriver(md);
+    public static Connection getConnection() throws SQLException, FileNotFoundException, IOException {
+        Driver driver = new com.mysql.jdbc.Driver();
+        DriverManager.registerDriver(driver); // in netbeans we do not need this 
 
+        Properties connectDB = new Properties();
+        FileInputStream info = new FileInputStream("jdbcConnect.txt");
+        connectDB.load(info);
+        Connection connect = DriverManager.getConnection(connectDB.getProperty("urlDB"), connectDB.getProperty("userDB"), connectDB.getProperty("password"));
+        // here we read URLDB , user , password frpm file <NameFile.txt> and write this info into getConnection method
+
+        return connect;
     }
 
-    public void readAll() throws SQLException {
+    public void readAll() throws SQLException, IOException { // this method to read all records and print it
 
-        String selectQuery = "select * from StudentsTBL_ahmed_ahmed";
-        PreparedStatement statement = connect.prepareStatement(selectQuery);
+        String Query = "select * from StudentsTBL_ahmed_ahmed"; // this query to connect with MYSQL
+        PreparedStatement statement = getConnection().prepareStatement(Query); //send query to MYSQL
         ResultSet printResult = statement.executeQuery();
-        System.out.printf(" %-5s  %-40s  %-15s  %4s %n", "ID", "NAME", "DATE", "GPA");
+        System.out.printf(" %-5s  %-40s  %-15s  %4s %n", "ID", "NAME", "DATE", "GPA"); // format to print the records like table
         System.out.println();
         System.out.println("-------------------------------------------------------------------------------------");
 
-        while (printResult.next()) {
+        while (printResult.next()) { // while loop To go through all the records and print them
             System.out.printf(" %-5s  %-40s  %-15s  %4.2f %n", printResult.getInt(1), printResult.getString(2), printResult.getDate(3), printResult.getDouble(4));
-
             System.out.println();
-
         }
         statement.close();
+    } // end while
 
-// end while
+    public void addStudent(String fullName, String dates, Double GPA) throws SQLException, IOException { //this method to add student in database
+
+        String Query = "Insert into StudentsTBL_Ahmed_Ahmed (FullName,DateOfBirth,GPA) values(?,?,?)"; // this query to connect with MYSQL
+        PreparedStatement statement = getConnection().prepareStatement(Query); //send query to MYSQL
+        statement.setString(1, fullName); // set name in first <?>
+        Date date = Date.valueOf(dates);
+        statement.setDate(2, date); // set date in second <?>
+        statement.setDouble(3, GPA); // set GPA in third <?>
+        int i = statement.executeUpdate();
+        System.out.println("Student record Inserted Successfully..."); // if everything is OK print this message
+        statement.close();
     }
 
-    public void addStudent(String fullName, String dat, Double GPA) throws SQLException {
-
-        String sql = "Insert into StudentsTBL_Ahmed_Ahmed (FullName,DateOfBirth,GPA) values(?,?,?)";
-        PreparedStatement pStmt = connect.prepareStatement(sql);
-        pStmt.setString(1, fullName);
-        Date date = Date.valueOf(dat);
-
-        pStmt.setDate(2, date);
-
-        pStmt.setDouble(3, GPA);
-        int i = pStmt.executeUpdate();
-        System.out.println("Student record Inserted Successfully...");
-        pStmt.close();
-    }
-
-    public void search() throws SQLException {
-        System.out.println("Enter Full Name:");
-        String fullName = scan.nextLine();
-
+    public void search() throws SQLException, IOException { // this method to search  in database
+        System.out.println("Enter your search:");
+        String search = scan.nextLine();
         String searchquery = "SELECT * FROM StudentsTBL_ahmed_ahmed WHERE FullName LIKE ? ";
-
-        PreparedStatement statement = connect.prepareStatement(searchquery);
-        fullName = "%" + fullName + "%";
-        statement.setString(1, fullName);
+        PreparedStatement statement = getConnection().prepareStatement(searchquery); //send query to MYSQL
+        search = "%" + search + "%";
+        statement.setString(1, search);  // set user input in first <?>
         ResultSet searchResult = statement.executeQuery();
-//        searchResult.isBeforeFirst()
         if (!searchResult.isBeforeFirst()) {
+            // check If  does not find records print message
             System.out.println("there is no records available for this search criteria");
             return;
         }
@@ -91,61 +87,58 @@ class connectDB {
         while (searchResult.next()) {
             System.out.printf(" %-5s  %-40s  %-15s  %4s %n", searchResult.getInt(1), searchResult.getString(2), searchResult.getDate(3), searchResult.getDouble(4));
             System.out.println();
-        }
+        }// end while
         statement.close();
     }
 
-    public String checkName() {
+    public String checkName() { // this method to check name and handle any problem if found
         String NAMES_PATTER = "[A-Za-z ]{3,40}";
+        // we use REGEX To determine Name pattern and character limit >> the name most be letters from 3  - 40 characters
         System.out.println("Enter Full Name:");
         String name = scan.nextLine().trim();
-        while (!name.matches(NAMES_PATTER)) {
+        while (!name.matches(NAMES_PATTER)) { // check if there any problem and print message about it
             if (name.length() > 40 || name.length() < 3) {
                 System.out.println("your name must be between 3 - 40 letters !!! ");
                 System.out.println("Enter Full Name:");
-                name = scan.nextLine();
+                name = scan.nextLine().trim();
             }
             System.out.println("your name must contain letters only !!!");
             System.out.println("Enter Full Name:");
-            name = scan.nextLine();
-        }
+            name = scan.nextLine().trim();
+        } // end while
         return name;
     }
 
-    public String checkDate() {
+    public String checkDate() { // this method to check Date and handle any problem if found
 
         String DATE_PATTER = "((18|19|20)[0-9]{2}[-](0?[13578]|1[02])[-](0?[1-9]|[12][0-9]|3[01]))|"
                 + "(18|19|20)[0-9]{2}[-](0?[469]|11)[-](0?[1-9]|[12][0-9]|30)|"
                 + "(18|19|20)[0-9]{2}[-](0?[2])[-](0?[1-9]|1[0-9]|2[0-8])|"
                 + "(((18|19|20)(04|08|[2468][048]|[13579][26]))|2000)[-](0?[2])[-]29";
+        // we use REGEX To determine Date  pattern YYYY-MM-DD
 
         System.out.println("Enter birth date in yyyy-mm-dd form:");
 
         String date = scan.nextLine();
-        while (!date.matches(DATE_PATTER)) {
+        while (!date.matches(DATE_PATTER)) {  // check if there any problem and print message about it
             System.out.println("Enter correct birth date in yyyy-mm-dd form:");
             date = scan.nextLine();
-
-        }
-
+        } // end while
         return date;
     }
 
-    public double checkGPA() {
+    public double checkGPA() { //this method to check GPA and handle any problem if found
         System.out.println("Enter student GPA \"must be between 0-4\"");
         String GPA_PATTER = "[4](.[0]+)?|([0-3])([.][0-9]+)?";
+        // we use REGEX To determine GPA  pattern 
         String GPA = scan.nextLine();
-
-        while (!GPA.matches(GPA_PATTER)) {
+        while (!GPA.matches(GPA_PATTER)) { // check if there any problem and print message about it
             System.out.println("Enter correct student GPA \"must be between 0-4\"");
             GPA = scan.nextLine();
-
-        }
+        } // end while
         if (GPA.length() > 4) {
             GPA = GPA.substring(0, 4);
         }
-
         return Double.parseDouble(GPA);
-
     }
 }
